8e0722877a13046607eaeafb3a021f085680d14c
Alan Rios <riosalan264@gmail.com>
2024-05-23 10:34:02 -0300

    Error en mi archivos json-server-ks

--- netlify/functions/json-server.js
+++ netlify/functions/json-server.js
@@ -1,6 +1,6 @@
 // netlify/functions/json-server.js
 
-const { createServer } = require('json-server');
+import { createServer } from 'json-server';
 const server = createServer();
 const router = server.router('db.json'); // tu archivo JSON con los datos
 const middlewares = server.defaults();
@@ -8,7 +8,7 @@ const middlewares = server.defaults();
 server.use(middlewares);
 server.use(router);
 
-module.exports.handler = async (event, context) => {
+export async function handler(event, context) {
   const response = await server.handleRequest(event, context);
   return response;
-};
+}
