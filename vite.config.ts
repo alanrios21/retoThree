70fb48c11613eccd697a3b5db1b99b377abb38ea
Alan Rios <riosalan264@gmail.com>
2024-05-31 17:34:44 -0300

    Actualize mi vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js o vite.config.mjs
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Agrupa react y react-dom en un fragmento separado
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Ajusta el límite de tamaño a 1000 kB
+  },
+});
